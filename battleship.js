56edc0303ac19a69b03488a69dc4959b7f5f76b2
Renato Guimaraes Nunes <renatognunes@hotmail.com>
2018-09-10 21:07:11 -0400

    Add model object

--- battleship.js
+++ battleship.js
@@ -11,4 +11,42 @@ var view = {
         var cell = document.getElementById(location);
         cell.setAttribute('class', 'miss');
     }
-}
+};
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+             { locations: ["24", "34", "44"], hits: ["", "", ""] }, 
+             { locations: ["10", "11", "12"], hits: ["", "", ""] } ],
+
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess); 
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("You have sank my battleship!");
+                if (this.isSunk(ship)) {
+                    this.shipsSunk++;
+                }
+                return true; 
+            }
+        }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+    },
+
+    isSunk: function(ship) {
+        for(var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    },
+};
